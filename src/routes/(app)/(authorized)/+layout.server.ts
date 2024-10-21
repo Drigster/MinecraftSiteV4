a7e740f5ce712dc3b90f2a7c9aa423b8c6253693
Drigster <62962772+Drigster@users.noreply.github.com>
2024-10-21 19:58:28 +0300

    fix notverified page

--- src/routes/(app)/(authorized)/+layout.server.ts
+++ src/routes/(app)/(authorized)/+layout.server.ts
@@ -1,5 +1,9 @@
 import { redirect } from "@sveltejs/kit";
 
 export async function load({ url, locals }) {
-	if (locals.user == null) redirect(302, "/login?redirectTo=" + url.pathname);
+	if (locals.user == null) {
+		redirect(302, "/login?redirectTo=" + url.pathname);
+	} else if (!locals.user.verified) {
+		redirect(302, "/notverified?redirectTo=" + url.pathname);
+	}
 }
