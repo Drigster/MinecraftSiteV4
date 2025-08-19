f3e040ea1f45d7e76258da812ba65fc229642f60
Drigster <62962772+Drigster@users.noreply.github.com>
2025-08-20 02:02:18 +0300

    fix download url

--- src/routes/(app)/download/+page.server.ts
+++ src/routes/(app)/download/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from "@sveltejs/kit";
 
 export const load = async () => {
-	return redirect(303, "https://launcher.foxy.town/FoxyTown Launcher.exe");
+	return redirect(303, "https://launcher.foxy.town/FoxyTown.exe");
 };
