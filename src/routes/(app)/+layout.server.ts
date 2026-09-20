22e75bff6eebe6c8b1b33681097a83e3d2c1913c
Gabriel Jõe <62962772+Drigster@users.noreply.github.com>
2026-09-20 16:17:54 +0300

    add under_construction page

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,4 +1,10 @@
-export async function load({ locals }) {
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ locals, cookies }) {
+	if (cookies.get("wip") != "off") {
+		return redirect(307, "/under_construction");
+	}
+
 	return {
 		user: locals.user,
 	};
---
+++ src/routes/under_construction/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>🚧🚧🚧 | Foxy.town</title>
+</svelte:head>
+
+<div class="timer">
+	<span class="countdown font-mono text-7xl">🚧 Скоро... 🚧</span>
+</div>
+
+<style>
+	.timer {
+		display: grid;
+		place-items: center;
+		height: 100vh;
+		height: 100svh;
+		text-align: center;
+		font-size: 10rem;
+	}
+</style>
